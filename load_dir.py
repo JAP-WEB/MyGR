9fce73c6e157831cc529a1e8c9fc5641e4d60fed
Jesus Elias Martinez <l20211768@tectijuana.edu.mx>
2024-03-19 01:42:37 +0000

    Actualizado

--- load_dir.py
+++ load_dir.py
@@ -22,14 +22,14 @@ def load_dir(path):
                 r.set(book_id, html) #almacena estable el valor de ID como clave en redis 
             print(f"{file} loaded into Redis") #mensaje de confirmación de la carga de libros
 
-#ini=dice de palabras de los html (libros)
+#índice de palabras de los html (libros)
 def create_index(book_id, html):
     soup = BeautifulSoup(html, 'html.parser') #crea un objeto que contiene la estructura del html
-    ts = str(soup.p).lower() #obtiene el contenido de <p></p> y lo convierte a minúsculas
-    palas = ts.split() #divide las palabras por espacios 
-    for t in palas:
-        t = t.replace(",","") #reemplaza/borra las comas del final de la palabra
-        r.sadd(t,book_id) #agrega el identificador y añade la claves al índice de mapeo
+    palabras = str(soup.p).lower() #obtiene el contenido de <p></p> y lo convierte a minúsculas
+    claves = palabras.split() #divide las palabras por espacios 
+    for clave in claves:
+        clave = clave.replace(",","") #reemplaza/borra las comas del final de la palabra
+        r.sadd(clave,book_id) #agrega el identificador y añade la claves al índice de mapeo
     
 #ejecución del método para cargar los html (libros)
 load_dir('html/books/')
--- webserver.py
+++ webserver.py
@@ -2,12 +2,12 @@ from functools import cached_property #almacena una prodiedad en caché tras cal
 from http.cookies import SimpleCookie #creación y manejo de cookies HTTP
 import re #librería de expresiones regulares
 #manejar solicutudes tipo HTTP de un servidor
-#crear un servidor HTTP, para escuchar y manejar las solicitudes entrantes
+#crear un servidor HTTP para escuchar y manejar las solicitudes entrantes
 from http.server import BaseHTTPRequestHandler, HTTPServer 
 #analizar las cadenas de consulta
 #analizar los componentes de los URL
 from urllib.parse import parse_qsl, urlparse 
-import redis #conexión con base de datos REDIS
+import redis #conexión con base de datos redis
 import uuid #manipula los UUID - identificadores únicos universales
 import os #comuincación con el sistema operativo
 import urllib.parse #división y creación de URL en base a los componentes
@@ -19,7 +19,8 @@ import urllib.parse #división y creación de URL en base a los componentes
 # https://docs.python.org/3/library/http.cookies.html
 #--------------------------------------------------------------------------#
 
-#Conexión local con REDIS
+#establecer conexión tipo local con la base de datos Redis
+#db especifica el número de base de datos en la cual se hará la conexión
 r = redis.Redis(host="localhost", port = 6379, db = 0)
 
 # definición de tuplas de mapeo, patrón de URL y llamada a la función 
@@ -36,7 +37,7 @@ mappings = [
 #declaración de clase, hereda las propiedades del módulo BaseHTTPRequestHandler
 class WebRequestHandler(BaseHTTPRequestHandler):
     
-    #declaración de función para las solicitudes de búsqueda 
+    #declaración de método para las solicitudes de búsqueda 
     def search(self):
         #verificación que se haya mandado una búsqueda 
         #'q' es donde se almacena la consulta de búsqueda
@@ -44,24 +45,24 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             query = self.query_data['q'] #asignar contenido de consulta 
             #busca en redis las palabras de manera indivual 'split'
             #sinter: devuelve los miembros del conjunto resultante de la intersección dada
-            booksB = r.sinter(query.split(' ')) #busca los libros que tengan las palabras de la consulta
-            lista = [] #declaración de lista
+            books = r.sinter(query.split(' ')) #busca los libros que tengan las palabras de la consulta
+            lista_libros = [] #declaración de lista
             #iteración para crear una lista de libros encontrados
-            for b in booksB:
-                y = b.decode() #decodificar la cadena
-                lista.append(y) #agregar a lista
-                print(lista) #imprimir lista (consola)
+            for b in books:
+                cadena = b.decode() #decodificar la cadena
+                lista_libros.append(cadena) #agregar a lista
+                print(lista_libros) #imprimir lista (consola)
             #mostrar resultados de búsqueda de libros
-            for i in range(0, len(lista)):
-                if i<len(lista):
-                    self.get_book(lista[i]) 
+            for i in range(0, len(lista_libros)):
+                if i<len(lista_libros):
+                    self.get_book(lista_libros[i]) 
                 else:
                     self.index()            
         #respuesta HTTP 
         self.send_response(200) #estado de éxito
         self.send_header('Content-type', 'text/html') #cabeceras de contenido
         self.end_headers()
-   
+        
     @property #definir a la función como propiedad de la clase
     #self: objeto de la clase
     def query_data(self):
@@ -70,58 +71,58 @@ class WebRequestHandler(BaseHTTPRequestHandler):
      
     @property  
     def url(self):
-        #divide el URL devuelve y devuelve un objeto con el resultado
+        #divide el URL y devuelve un objeto con el resultado
         return urlparse(self.path)
    
     def get_method(self, path):
-        #iteración de la ruta y el metodo en mappings
+        #iteración de la ruta y el método en mappings
         for pattern, method in mappings:
             #valida la ruta con la expresión regular 
             match = re.match(pattern, path)
             if match:
-                #devuelve el metodo y un diccionario de rutas coincidentes
+                #devuelve el método y un diccionario de rutas coincidentes
                 return (method, match.groupdict())
-
+   
+   #obtiene lista de libros asociados a la sesion, limitando el rango de libros
     def get_recomendation(self,session_id,book_id):
-        #insertar los valores a la lista (redis)
         r.rpush(session_id,book_id)
-        #devolver los elementos especificados almacenados en keys (redis)
-        books = r.lrange(session_id,0 ,-1)
-        print(session_id,books) #imprime resultados
+        books = r.lrange(session_id,0 ,6)
+        print(session_id,books)
+        library = [str(i+1) for i in range(6)] # crea etiquetas de libros (str-cadenas)
         
-        #creacion de lista de enumeración de los libros 
-        library = [str(i+1) for i in range(6)]
-        #
+        # crea lista de recs, revisa los libros leidos, sino los agrega a lista de recs
         recomendation = [book for book in library if book not in
                         [read.decode() for read in books]]
         
-        if len(recomendation) > 3:  # condición a > 3 para que recomiende después del segundo
-            return recomendation[2]  # se devuelve el tercer libro de la lista de recomendaciones
+        # Recomienda despues del segundo libro, devolviendo la rec del 3
+        if len(recomendation) > 3:  
+            return recomendation[2] 
+        # Si hay al menos 1 libro en recs, devolver el 1er libro de la lista
         elif len(recomendation) > 0:
-            return recomendation[0] #devuelve el primer libro de la lista de recomendaciones
+            return recomendation[0]
         else:
-            return "No hay recomendaciones"
+            return "No hay recomendaciones" # No quedan libros en la lista
    
     def get_book(self, book_id):
-        session_id = self.get_session() #obtiene el session_id llamando al la función get_session
-        get_recomendation = self.get_recomendation(session_id, book_id) #obtiene el la recomendació llamando al la función get_recomendation
-        Indice = r.get(book_id) #drea una pagina con el libro de acuerdo al book_id obtenido de redis
-        if Indice:
-            #despliegue de la pagina
+        session_id = self.get_session() #obtiene el session_id llamando al método get_session
+        get_recomendation = self.get_recomendation(session_id, book_id) #obtiene la recomendación llamando al método get_recomendation
+        pagina = r.get(book_id) #crea una página con el libro de acuerdo al book_id obtenido de redis
+        if pagina:
+            #despliegue de la página
             self.send_response(200)
             self.send_header("Content-Type", "text/html")
             self.write_session_cookie(session_id) #escribe las cookies de la sesión
             self.end_headers()
             #despliegue de datos de sesión y recomendaciones
             response = f"""
-            {Indice.decode()}
+            {pagina.decode()}
             <p>  Id-session: {session_id} </p>
             <p>  Te recomendamos leer: {get_recomendation} </p>
             """
             self.wfile.write(response.encode("utf-8"))
         else:
             self.send_error(404, "Not Found") #mensaje de error en caso de que no exista libro
-     
+    
     #función para tomar las cookies de las cabeceras de HTTP   
     def cookies(self):
         return SimpleCookie(self.headers.get("Cookie"))
@@ -140,31 +141,28 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     #establecer cookies a la sesión actual   
     def write_session_cookie(self, session_id):
         cookies = SimpleCookie() #crea un objeto del tipo SimpleCookie
-        cookies["session_id"] = session_id #estable la cookie a la sesión actual
+        cookies["session_id"] = session_id #establece la cookie a la sesión actual
         cookies["session_id"]["max-age"] = 1000 #tiempo de expiración de la cookie
-        self.send_header("Set-Cookie", cookies.output(header="")) #envia la cookie como header
-
+        self.send_header("Set-Cookie", cookies.output(header="")) #envía la cookie como header
+   
     #manejo de solicitudes HTTP GET
-    #funcion GET que solicita al servidor que te de la informacion al abrir el navegador 
-    #obtiene el metodo que se esta solictando, tomando el path de la URL como argumento
-    #obtiene el nombre del metodo y lo llama pasando los parametros del diccionario. 
     def do_GET(self):
         method = self.get_method(self.url.path)
-        if method:  # Verifica si encuentra el metodo
+        if method:  #verifica si encuentra el metodo
             method_name, dict_params = method
             method = getattr(self, method_name)
             method(**dict_params) # ** = expande los argumentos del diccionario
             return
         else:
-            # si no se encontro ningun metodo, el servidor manda error 404
+            #si no se encontro ningun metodo, el servidor manda error 404
             self.send_error(404, "Not Found")
             
-       
+            
     def index(self):
-        session_id = self.get_session() #obtener el session_id llamando a la función get_session
+        session_id = self.get_session() #obtener el session_id llamando al método get_session
         self.send_response(200) #respuesta HTTP
         self.send_header("Content-Type", "text/html")
-        self.write_session_cookie(session_id) #llama a la función write_session_cookie y le pasa el session_id
+        self.write_session_cookie(session_id) #llama al método write_session_cookie y le pasa el session_id
         self.end_headers()
         #abre el archivo index.html
         with open('html/index.html') as f:
